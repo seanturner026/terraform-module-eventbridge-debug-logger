6223ec0a22afb86ee9fb53fcdc4cadc12be156a4
Sean Turner <sean.turner026@gmail.com>
2021-04-25 17:02:32 +1200

    Added var.name to iam resources

--- r_iam.tf
+++ r_iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "this" {
-  name               = "${local.service_name}_events_debug_lambda_logger_role"
+  name               = "${var.name}_${local.service_name}_events_lambda_logger_role"
   assume_role_policy = data.aws_iam_policy_document.this.json
   tags               = var.tags
 }
